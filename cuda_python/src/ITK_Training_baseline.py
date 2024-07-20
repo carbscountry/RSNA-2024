4569bbd5b71b50a1ca849022852d200bcad5ad71
carbscountry <s1270161forbisness@gmail.com>
2024-07-20 12:19:18 +0900

    src

--- cuda_python/src/ITK_Training_baseline.py
+++ cuda_python/src/ITK_Training_baseline.py
@@ -1,6 +1,6 @@
 # https://www.kaggle.com/code/itsuki9180/rsna2024-lsdc-training-baseline
 
-import os
+import os,re,sys
 import gc
 import sys
 import time
@@ -9,7 +9,7 @@ import cv2
 import math, random
 import numpy as np
 import pandas as pd
-from glob import glob
+import glob
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
@@ -29,7 +29,7 @@ import albumentations as A
 
 from sklearn.model_selection import KFold
 
-import wandb
+import pydicom
 
 ### set path 
 
@@ -47,6 +47,7 @@ device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 IN_CHANS = 30
 SEED = 8620
+IMG_SIZE = [512, 512]
 
 ### set random seed
 
@@ -100,7 +101,7 @@ class RSNA24Dataset(Dataset):
                 pass
             
         # Axial T2
-        axt2 = glob(os.path.join(cvt_path,f'{st_id}/Axial T2/*.png'))
+        axt2 = glob.glob(os.path.join(cvt_path,f'{st_id}/Axial T2/*.png'))
         axt2 = sorted(axt2)
     
         step = len(axt2) / 10.0
@@ -125,7 +126,109 @@ class RSNA24Dataset(Dataset):
         x = x.transpose(2, 0, 1)
                 
         return x, label
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
     
+class RSNA24TestDataset(Dataset):
+    def __init__(self, df, study_ids, phase='test', transform=None):
+        self.df = df
+        self.study_ids = study_ids
+        self.transform = transform
+        self.phase = phase
+    
+    def __len__(self):
+        return len(self.study_ids)
+    
+    def get_img_paths(self, study_id, series_desc):
+        pdf = self.df[self.df['study_id']==study_id]
+        pdf_ = pdf[pdf['series_description']==series_desc]
+        allimgs = []
+        for i, row in pdf_.iterrows():
+            pimgs = glob.glob(f'{input_path}/test_images/{study_id}/{row["series_id"]}/*.dcm')
+            pimgs = sorted(pimgs, key=natural_keys)
+            allimgs.extend(pimgs)
+            
+        return allimgs
+    
+    def read_dcm_ret_arr(self, src_path):
+        dicom_data = pydicom.dcmread(src_path)
+        image = dicom_data.pixel_array
+        image = (image - image.min()) / (image.max() - image.min() + 1e-6) * 255
+        img = cv2.resize(image, (IMG_SIZE[0], IMG_SIZE[1]),interpolation=cv2.INTER_CUBIC)
+        assert img.shape==(IMG_SIZE[0], IMG_SIZE[1])
+        return img
+
+    def __getitem__(self, idx):
+        x = np.zeros((IMG_SIZE[0], IMG_SIZE[1], IN_CHANS), dtype=np.uint8)
+        st_id = self.study_ids[idx]        
+        
+        # Sagittal T1
+        allimgs_st1 = self.get_img_paths(st_id, 'Sagittal T1')
+        if len(allimgs_st1)==0:
+            print(st_id, ': Sagittal T1, has no images')
+        
+        else:
+            step = len(allimgs_st1) / 10.0
+            st = len(allimgs_st1)/2.0 - 4.0*step
+            end = len(allimgs_st1)+0.0001
+            for j, i in enumerate(np.arange(st, end, step)):
+                try:
+                    ind2 = max(0, int((i-0.5001).round()))
+                    img = self.read_dcm_ret_arr(allimgs_st1[ind2])
+                    x[..., j] = img.astype(np.uint8)
+                except:
+                    print(f'failed to load on {st_id}, Sagittal T1')
+                    pass
+            
+        # Sagittal T2/STIR
+        allimgs_st2 = self.get_img_paths(st_id, 'Sagittal T2/STIR')
+        if len(allimgs_st2)==0:
+            print(st_id, ': Sagittal T2/STIR, has no images')
+            
+        else:
+            step = len(allimgs_st2) / 10.0
+            st = len(allimgs_st2)/2.0 - 4.0*step
+            end = len(allimgs_st2)+0.0001
+            for j, i in enumerate(np.arange(st, end, step)):
+                try:
+                    ind2 = max(0, int((i-0.5001).round()))
+                    img = self.read_dcm_ret_arr(allimgs_st2[ind2])
+                    x[..., j+10] = img.astype(np.uint8)
+                except:
+                    print(f'failed to load on {st_id}, Sagittal T2/STIR')
+                    pass
+            
+        # Axial T2
+        allimgs_at2 = self.get_img_paths(st_id, 'Axial T2')
+        if len(allimgs_at2)==0:
+            print(st_id, ': Axial T2, has no images')
+            
+        else:
+            step = len(allimgs_at2) / 10.0
+            st = len(allimgs_at2)/2.0 - 4.0*step
+            end = len(allimgs_at2)+0.0001
+
+            for j, i in enumerate(np.arange(st, end, step)):
+                try:
+                    ind2 = max(0, int((i-0.5001).round()))
+                    img = self.read_dcm_ret_arr(allimgs_at2[ind2])
+                    x[..., j+20] = img.astype(np.uint8)
+                except:
+                    print(f'failed to load on {st_id}, Axial T2')
+                    pass  
+            
+            
+        if self.transform is not None:
+            x = self.transform(image=x)['image']
+
+        x = x.transpose(2, 0, 1)
+                
+        return x, str(st_id)
+
 class RSNA24Model(nn.Module):
     def __init__(self, model_name, in_c=30, n_classes=75, pretrained=True, features_only=False):
         super().__init__()
